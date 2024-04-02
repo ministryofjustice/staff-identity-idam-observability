930e57448ccd672903921a631fe1ad1d1a9e6b70
John Nolan <john.nolan@justice.gov.uk>
2024-04-02 13:29:43 +0100

    Rename files

--- terraform/resource-group.tf
+++ terraform/resource-group.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "resource_group" {
   location = var.location
-  name     = "${var.project}-${var.environment}-resource-group"
+  name     = "rg-eucs-${var.project}-${var.environment}-resource-group"
 
   tags = local.tags
 }
