c4ed248f6acd0e2686694ea257e4a431e390e499
John Nolan <john.nolan@justice.gov.uk>
2024-04-03 08:58:17 +0100

    Prefix log workspace

--- terraform/log-analytics-workspace.tf
+++ terraform/log-analytics-workspace.tf
@@ -1,5 +1,5 @@
 resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
-  name                = "${var.department}${var.team}${var.project}workspace"
+  name                = "log-${var.department}${var.team}${var.project}"
   location            = var.location
   resource_group_name = local.rg_name
 
