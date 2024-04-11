a5f3d9fce49c10d4b404cb61f90ec29280db1865
John Nolan <john.nolan@justice.gov.uk>
2024-04-11 16:22:53 +0100

    Add azure blob backend state

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -9,6 +9,12 @@ terraform {
       version = "~>1.12.1"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-eucs-idam-observability"
+    storage_account_name = "stidamobservetfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
