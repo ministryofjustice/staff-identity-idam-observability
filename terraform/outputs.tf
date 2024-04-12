f7ea66484d2e0c59572f31715cc0e1005438bedc
John Nolan <john.nolan@justice.gov.uk>
2024-04-12 16:03:12 +0100

    Tidied outputs file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,26 +2,22 @@ output "resource_group_name" {
   value = local.rg_name
 }
 
-output "client_id" {
-  value = azurerm_user_assigned_identity.managed_identity.client_id
-}
-
-output "logs_ingestion_endpoint" {
-  value = azurerm_monitor_data_collection_endpoint.data_collection_endpoint.logs_ingestion_endpoint
-}
-
 output "configuration_access_endpoint" {
   value = azurerm_monitor_data_collection_endpoint.data_collection_endpoint.configuration_access_endpoint
 }
 
-output "dcr_id" {
-  value = azurerm_monitor_data_collection_rule.data_collection_rule.id
+output "managed_identity_client_id" {
+  value = azurerm_user_assigned_identity.managed_identity.client_id
 }
 
 output "dcr_immutable_id" {
   value = azurerm_monitor_data_collection_rule.data_collection_rule.immutable_id
 }
 
-output "workspace_table_name" {
+output "log_table_name" {
   value = azapi_resource.workspaces_table.name
+}
+
+output "dceuri" {
+  value = azurerm_monitor_data_collection_endpoint.data_collection_endpoint.logs_ingestion_endpoint
 }
