5e2b84224d26192ac91cbb64a640dcaf2464a0ab
John Nolan <john.nolan@justice.gov.uk>
2025-01-21 15:44:20 +0000

    Remove managed job schedule

--- terraform/automation-account.tf
+++ terraform/automation-account.tf
@@ -28,17 +28,3 @@ resource "azurerm_automation_schedule" "automation_schedule" {
   start_time              = "2025-01-15T07:00:00+01:00"
   description             = "Run export daily."
 }
-
-resource "azurerm_automation_job_schedule" "example" {
-  resource_group_name     = local.rg_name
-  automation_account_name = azurerm_automation_account.automation_account.name
-  schedule_name           = azurerm_automation_schedule.automation_schedule.name
-  runbook_name            = azurerm_automation_runbook.runbook.name
-
-  parameters = {
-    miclientid     = azurerm_user_assigned_identity.managed_identity.client_id
-    dcrimmutableid = azurerm_monitor_data_collection_rule.data_collection_rule.immutable_id
-    logtablename   = azapi_resource.workspaces_table.name
-    dceuri         = azurerm_monitor_data_collection_endpoint.data_collection_endpoint.logs_ingestion_endpoint
-  }
-}
