0ddade9408acb70cc30ef1eb35149bb4062bfae5
MikeT-MoJ <michael.tulett@justice.gov.uk>
2025-01-30 13:22:01 +0000

    Update scripts/ps/process-app-registration-creds-cleanup.ps1
    Co-authored-by: jasegillettmoj <jason.gillett@justice.gov.uk>

--- scripts/ps/process-app-registration-creds-cleanup.ps1
+++ scripts/ps/process-app-registration-creds-cleanup.ps1
@@ -147,7 +147,7 @@ $ExpiredCredsTrimmed = $ExpiredCreds | sort daystoexpiration | select -First 1
     $clean = foreach ($ExpiredCred in $ExpiredCredsTrimmed){
         write-host "Will remove cred from $(($ExpiredCred).displayname), keyID $(($ExpiredCred).keyid)."
             $ErrorActionPreference = "stop"
-            try
+            try {
                 {Remove-AzADAppCredential -DisplayName $ExpiredCred.displayname -KeyId $ExpiredCred.keyid -ErrorAction stop
                 $removal = "Removed"
                 }
