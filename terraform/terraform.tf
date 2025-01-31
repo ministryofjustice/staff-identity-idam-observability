d6a2eb4a4043d442683b5514856084145c9fa68e
MikeT-MoJ <michael.tulett@justice.gov.uk>
2025-01-31 09:45:20 +0000

    Idam 1890 custom role (#44)
    * added azuread provider
    
    * added azuread custom role

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -26,6 +26,12 @@ provider "azurerm" {
   client_secret   = var.client_secret
 }
 
+provider "azuread" {
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
 provider "azapi" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
