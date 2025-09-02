3427ce6ce79e24da152e8f522682a5cdf3346ed0
Fairfield <lee.fairfield@justice.gov.uk>
2025-09-02 09:29:03 +0100

    IDAM-2081 Adding new AccessPackage table to log analytics workspace

--- terraform/log-analytics-workspace-table.tf
+++ terraform/log-analytics-workspace-table.tf
@@ -64,6 +64,72 @@ resource "azapi_resource" "workspaces_table" {
   ]
 }
 
+resource "azapi_resource" "workspaces_table_access_package_info" {
+  type      = "Microsoft.OperationalInsights/workspaces/tables@2021-12-01-preview"
+  name      = "${var.department}_${var.team}_${var.project}_AccessPackage_CL"
+  parent_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+
+  body = jsonencode({
+    properties = {
+      plan = "Analytics",
+      schema = {
+        name = "${var.department}_${var.team}_${var.project}_AccessPackage_CL",
+        columns = [
+          {
+            name = "displayname",
+            type = "string"
+          },
+          {
+            name = "objectid",
+            type = "string"
+          },
+          {
+            name = "applicationid",
+            type = "string"
+          },
+          {
+            name = "keyid",
+            type = "string"
+          },
+          {
+            name = "eventtype",
+            type = "string"
+          },
+          {
+            name = "startdate",
+            type = "dateTime"
+          },
+          {
+            name = "enddate",
+            type = "dateTime"
+          },
+          {
+            name = "status",
+            type = "string"
+          },
+          {
+            name = "TimeGenerated",
+            type = "dateTime"
+          },
+          {
+            name = "daystoexpiration",
+            type = "int"
+          },
+          {
+            name = "owners",
+            type = "string"
+          }
+        ]
+      }
+    }
+  })
+  response_export_values = ["*"]
+
+  depends_on = [
+    azurerm_log_analytics_workspace.log_analytics_workspace
+  ]
+}
+
 resource "azapi_resource" "workspaces_table_creds_cleanup_script" {
   type      = "Microsoft.OperationalInsights/workspaces/tables@2021-12-01-preview"
   name      = "${var.department}_${var.team}_${var.project}_creds_cleanup_logs_CL"
