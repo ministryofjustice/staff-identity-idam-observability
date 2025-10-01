e148b6110255e71b66d90ad99ac1161e9829c9ad
John Nolan <john.nolan@justice.gov.uk>
2025-10-01 09:48:32 +0100

    Add schedules for
    * `automation_schedule_Access_Package`
    * `automation_schedule_guest_del`

--- terraform/automation-account.tf
+++ terraform/automation-account.tf
@@ -26,7 +26,7 @@ resource "azurerm_automation_schedule" "automation_schedule" {
   interval                = 1
   timezone                = "Europe/London"
   start_time              = "2025-10-01T07:00:00+01:00"
-  description             = "Run export daily."
+  description             = "Run daily application registration credential report."
 }
 
 resource "azurerm_automation_job_schedule" "automation_job_schedule" {
@@ -60,8 +60,8 @@ resource "azurerm_automation_schedule" "automation_schedule_guest_users" {
   frequency               = "Day"
   interval                = 1
   timezone                = "Europe/London"
-  start_time              = "2025-09-16T07:00:00+01:00"
-  description             = "Run export daily."
+  start_time              = "2025-10-01T07:00:00+01:00"
+  description             = "Run Guest User reporting daily."
 }
 
 resource "azurerm_automation_job_schedule" "automation_job_schedule_guest_users" {
@@ -86,8 +86,23 @@ resource "azurerm_automation_schedule" "automation_schedule_Access_Package" {
   frequency               = "Day"
   interval                = 14
   timezone                = "Europe/London"
-  start_time              = "2025-09-16T07:00:00+01:00"
+  start_time              = "2025-10-01T07:00:00+01:00"
   description             = "Run AccessPackage runbook every 2 weeks."
+}
+
+resource "azurerm_automation_job_schedule" "automation_job_schedule_Access_Package" {
+  count = var.workspace_name == "LIVE" ? 1 : 0
+
+  resource_group_name     = local.rg_name
+  automation_account_name = azurerm_automation_account.automation_account.name
+  runbook_name            = azurerm_automation_runbook.runbook_Access_Package_info_script.name
+  schedule_name           = azurerm_automation_schedule.automation_schedule_Access_Package.name
+  parameters = {
+    miclientid     = azurerm_user_assigned_identity.managed_identity.client_id,
+    dcrimmutableid = azurerm_monitor_data_collection_rule.data_collection_rule_AccessPackage.immutable_id,
+    dceuri         = azurerm_monitor_data_collection_endpoint.data_collection_endpoint.logs_ingestion_endpoint,
+    logtablename   = azapi_resource.workspaces_table_access_package_info.name
+  }
 }
 
 resource "azurerm_automation_schedule" "automation_schedule_guest_del" {
@@ -97,6 +112,21 @@ resource "azurerm_automation_schedule" "automation_schedule_guest_del" {
   frequency               = "Day"
   interval                = 1
   timezone                = "Europe/London"
-  start_time              = "2025-09-26T07:00:00+01:00"
+  start_time              = "2025-10-01T07:00:00+01:00"
   description             = "Run Guest User Delete every day."
+}
+
+resource "azurerm_automation_job_schedule" "automation_job_schedule_guest_del" {
+  count = var.workspace_name == "DEVLEXTERNAL" ? 1 : 0
+
+  resource_group_name     = local.rg_name
+  automation_account_name = azurerm_automation_account.automation_account.name
+  runbook_name            = azurerm_automation_runbook.runbook_guest_del_devl.name
+  schedule_name           = azurerm_automation_schedule.automation_schedule_guest_del.name
+  parameters = {
+    miclientid     = azurerm_user_assigned_identity.managed_identity.client_id,
+    dcrimmutableid = azurerm_monitor_data_collection_rule.data_collection_rule_guest_del.immutable_id,
+    dceuri         = azurerm_monitor_data_collection_endpoint.data_collection_endpoint.logs_ingestion_endpoint,
+    logtablename   = azapi_resource.workspaces_table_guest_del_script.name
+  }
 }
