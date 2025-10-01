1f6f45c99abfb4390149a95732b78bd093784cff
John Nolan <john.nolan@justice.gov.uk>
2025-10-01 09:27:09 +0100

    tf fmt

--- terraform/automation-account.tf
+++ terraform/automation-account.tf
@@ -34,7 +34,7 @@ resource "azurerm_automation_job_schedule" "automation_job_schedule" {
   automation_account_name = azurerm_automation_account.automation_account.name
   runbook_name            = azurerm_automation_runbook.runbook.name
   schedule_name           = azurerm_automation_schedule.automation_schedule.name
-  parameters = { 
+  parameters = {
     miclientid     = azurerm_user_assigned_identity.managed_identity.client_id,
     dcrimmutableid = azurerm_monitor_data_collection_rule.data_collection_rule.immutable_id,
     dceuri         = azurerm_monitor_data_collection_endpoint.data_collection_endpoint.logs_ingestion_endpoint,
