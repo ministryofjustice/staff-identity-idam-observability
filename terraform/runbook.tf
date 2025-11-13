84218018a475191ea01e71f544c094db9e29cdd8
Michael Tulett <Michael.Tulett@Justice.gov.uk>
2025-11-13 09:08:11 +0000

    Adds runbook

--- terraform/runbook.tf
+++ terraform/runbook.tf
@@ -111,3 +111,22 @@ resource "azurerm_automation_runbook" "runbook_mfa_metrics" {
 
   tags = local.tags
 }
+
+data "local_file" "user_metrics" {
+  filename = "${path.module}/../scripts/ps/get-user-metrics.ps1"
+}
+
+resource "azurerm_automation_runbook" "runbook_user_metrics" {
+  name                    = "rb-${var.department}-${var.team}-${var.project}-user-metrics-script"
+  location                = var.location
+  resource_group_name     = local.rg_name
+  automation_account_name = azurerm_automation_account.automation_account.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = "Retrieves user metrics."
+  runbook_type            = "PowerShell72"
+
+  content = data.local_file.user_metrics.content
+
+  tags = local.tags
+}
