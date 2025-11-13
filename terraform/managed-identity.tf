2a8a8da68af63be44aeee3a8ae8b0559bb855ee5
Michael Tulett <Michael.Tulett@Justice.gov.uk>
2025-11-13 09:51:32 +0000

    Added MI permissions

--- terraform/managed-identity.tf
+++ terraform/managed-identity.tf
@@ -94,3 +94,14 @@ resource "azurerm_role_assignment" "assign_identity_dcr_mfa_metrics_monitoring_p
     azurerm_automation_account.automation_account,
   ]
 }
+
+resource "azurerm_role_assignment" "assign_identity_dcr_user_metrics_monitoring_publisher" {
+  scope                = azurerm_monitor_data_collection_rule.data_collection_rule_user_metrics.id
+  role_definition_name = "Monitoring Metrics Publisher"
+  principal_id         = azurerm_user_assigned_identity.managed_identity.principal_id
+
+  depends_on = [
+    azurerm_user_assigned_identity.managed_identity,
+    azurerm_automation_account.automation_account,
+  ]
+}
