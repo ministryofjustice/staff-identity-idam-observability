d4ad4847a454888455a747b4bb8fe6cf8a22a5fd
Fairfield <lee.fairfield@justice.gov.uk>
2026-06-23 11:28:26 +0100

    Fixing terraform file format

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -34,10 +34,10 @@ provider "azurerm" {
 #}
 
 provider "azapi" {
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  use_oidc        = true
+  subscription_id  = var.subscription_id
+  tenant_id        = var.tenant_id
+  client_id        = var.client_id
+  use_oidc         = true
   default_tags     = local.tags
   default_location = var.location
 }
